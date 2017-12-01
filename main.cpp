cceeec4702c2428d65d8fc725cf92f9df26797e0
seth palmer <sethmpalmer@msn.com>
2017-11-30 18:33:43 -0700

    added project

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ int main()
     MyString big("big");
 
     string s1 = big.getInternalCString();
-    cout<<s1<<endl;
-
+    //cout<<s1<<endl;
+	/*
     MyString saw("saw");
     MyString bigger("bigger");
     MyString little("little");
@@ -21,23 +21,24 @@ int main()
     //Note, make sure you are calling your own comparestr() function,
     // and not the one included in the C libraries!
     cout << "comparestr(big, bigger) = "
-         << comparestr(big, bigger) << endl; //display a -1
+         << compareStr(big, bigger) << endl; //display a -1
     cout << "comparestr(bigger, big) = "
-         << comparestr(bigger, big) << endl; //display a 1
+         << compareStr(bigger, big) << endl; //display a 1
     cout << "comparestr(big, big) = "
-         << comparestr(big, big) << endl; //display a 0
+         << compareStr(big, big) << endl; //display a 0
     cout << "comparestr(big, little) = "
-         << comparestr(big, little) << endl; //display a -1
+         << compareStr(big, little) << endl; //display a -1
     cout << "comparestr(little, little) = "
-         << comparestr(little, little) << endl;  //display 0
+         << compareStr(little, little) << endl;  //display 0
     cout << "comparestr(littler, little) = "
-         << comparestr(littler, little) << endl; //display 1
+         << compareStr(littler, little) << endl; //display 1
     cout << "comparestr(big, saw) = "
-         << comparestr(saw, big) << endl; //display -2
+         << compareStr(saw, big) << endl; //display -2
 
     // This section will be used to test your
     // output operator "<<"
     //
+	
     cout <<"Original: "<< bigger << endl; //reggib
     bigger.reverseit();
     cout <<"New: "<< bigger << endl; //reggib
@@ -45,6 +46,7 @@ int main()
     cout <<"Original: "<< littler << endl; //reggib
     littler.reverseit();
     cout <<"New: "<< littler << endl; //reggib
-    
+	
+    */
     return 0;
 }
